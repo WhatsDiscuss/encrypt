37897b40aaf792ded2118425e30005e7016ffc86
WhatsDiscuss <support@whatsdiscuss.in>
2025-09-04 18:42:11 +0530

    Fixed dart analysis issues.

--- lib/src/algorithms/rsa.dart
+++ lib/src/algorithms/rsa.dart
@@ -1,4 +1,4 @@
-part of encrypt;
+part of '../../encrypt.dart';
 
 // Abstract class for encryption and signing.
 abstract class AbstractRSA {
@@ -21,7 +21,6 @@ abstract class AbstractRSA {
           RSAEngine(),
         );
       case RSADigest.SHA1:
-      default:
         return OAEPEncoding.withSHA1(RSAEngine());
     }
   }
